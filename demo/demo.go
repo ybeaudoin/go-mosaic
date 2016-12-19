9c2c3c662e458279f1d86c571a6efb25c83b01e3
ybeaudoin <webpraxis@gmail.com>
2016-12-18 17:50:32 -0700

    Add files via upload

--- demo/demo.go
+++ demo/demo.go
@@ -1,1 +1,9 @@
-
+package main
+import(
+    "github.com/ybeaudoin/go-mosaic"
+)
+func main() {
+    mosaic.Truchet("../images/Ada_Lovelace.jpg", "Ada_Lovelace-Truchet64.gif", 64)
+    mosaic.Truchet("../images/Ada_Lovelace.jpg", "Ada_Lovelace-Truchet32.png", 32)
+    mosaic.Truchet("../images/Ada_Lovelace.jpg", "Ada_Lovelace-Truchet16.jpg", 16)
+}
